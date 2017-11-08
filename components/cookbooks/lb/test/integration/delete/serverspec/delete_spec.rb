5f2e6a94c2eee28ba8a88f135ff550cb48b648e6
rameshjanjyam <rameshjanjyam@live.com>
2017-11-08 12:58:00 -0600

    tests for azure lb delete

---
+++ components/cookbooks/lb/test/integration/delete/serverspec/delete_spec.rb
@@ -0,0 +1,10 @@
+COOKBOOKS_PATH="/opt/oneops/inductor/circuit-oneops-1/components/cookbooks"
+
+require_relative 'spec_helper'
+require "#{COOKBOOKS_PATH}/azure_base/test/integration/azure_spec_utils"
+
+provider = AzureSpecUtils.new($node).get_provider
+if provider =~ /azure/
+  require_relative './azure/delete'
+end
+
